f34d3095e7bca86f810ceff0bd4bf159ce308c15
David Gonzalez <DavidGonzalez2001-9@hotmail.com>
2022-05-23 00:29:12 +0200

    Clean bug solving
    Drawing did not clean properly

--- Zenit Editor/Source/Panels/PanelViewport.cpp
+++ Zenit Editor/Source/Panels/PanelViewport.cpp
@@ -18,22 +18,22 @@ namespace Zenit {
 
 	void PanelViewport::OnImGuiRender(FrameBuffer* fbo, PerspectiveCamera camera, Model* model)
 	{
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+		//ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
 		ImGui::Begin("Viewport");
 		ImVec2 dimensions = ImGui::GetContentRegionAvail();
 		if (viewportSize.x != dimensions.x || viewportSize.y != dimensions.y)
 		{
 			fbo->Resize(dimensions.x, dimensions.y);
 			Renderer3D::OnResize(dimensions.x, dimensions.y);
-			camera.UpdateFov(dimensions.x, dimensions.y);
+			//camera.UpdateFov(dimensions.x, dimensions.y);
 			
 			ZN_CORE_TRACE("Viewport Resized");
 			viewportSize = { dimensions.x, dimensions.y };
 		}
 		
-		ImGui::Image((void*)fbo->GetColorId(), dimensions, { 0,1 }, { 1,0 });
+		ImGui::Image((void*)fbo->GetColorId(), { viewportSize.x, viewportSize.y }, { 0,1 }, { 1,0 });
 		ImGui::End();
-		ImGui::PopStyleVar();
+		//ImGui::PopStyleVar();
 	}
 
 }
--- Zenit/Source/Zenit/Renderer/Renderer3D.cpp
+++ Zenit/Source/Zenit/Renderer/Renderer3D.cpp
@@ -17,7 +17,7 @@ namespace Zenit {
 	void Renderer3D::Clear(const glm::vec4& color)
 	{
 		glClearColor(color.r, color.g, color.b, color.a);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_DEPTH_STENCIL);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	void Renderer3D::OnResize(int width, int height)
